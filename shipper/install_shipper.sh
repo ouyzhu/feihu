215f87c822a96b8ac96515c0613e1f32e512861b
stico <ouyzhu@gmail.com>
2014-01-07 18:47:38 +0800

    add script for git

--- shipper/install_shipper.sh
+++ shipper/install_shipper.sh
@@ -6,6 +6,9 @@ feihu_install=/data/apps/feihu_install
 [ ! -e $feihu ] && sudo mkdir -p $feihu && sudo chown $(whoami):$(whoami) $feihu
 [ ! -e $feihu_install ] && sudo mkdir -p $feihu_install && sudo chown $(whoami):$(whoami) $feihu_install
 
+# need git
+sudo apt-get install -y git
+
 # Setup nodejs
 node_url=http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-x64.tar.gz
 node_pkg=$feihu_install/node-v0.10.24-linux-x64.tar.gz
