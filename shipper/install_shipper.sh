4b4379d75629065f06ebdbf676ac872220b7d34a
stico <ouyzhu@gmail.com>
2014-01-07 17:34:58 +0800

    update control script

--- shipper/install_shipper.sh
+++ shipper/install_shipper.sh
@@ -71,9 +71,9 @@ if [ ! -e $stop_script ] ; then
 
 		echo "INFO: stopping shipper components (statsd/logstash)
 		statsd_pid=\$(func_get_pid "statsd")
-		[ -n "$statsd_pid"] && echo kill \$statsd_pid && kill \$statsd_pid
+		[ -n "\$statsd_pid"] && echo kill \$statsd_pid && kill \$statsd_pid
 		logstash_pid=\$(func_get_pid "logstash")
-		[ -n "$logstash_pid"] && echo kill \$logstash_pid && kill \$logstash_pid
+		[ -n "$\logstash_pid"] && echo kill \$logstash_pid && kill \$logstash_pid
 	EOF
 fi
 if [ ! -e $start_script ] ; then
